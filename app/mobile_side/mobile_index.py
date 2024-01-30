6b7309b059bd3087926d447f39187a226db5b882
KMantulak <103534147+MantulakK@users.noreply.github.com>
2024-01-30 10:57:30 -0300

    Delete app directory

--- app/mobile_side/mobile_index.py
+++
@@ -1,9 +0,0 @@
-from flask import *
-import sql_connection as db
-mobile_index_bp = Blueprint('mobile_index', __name__, url_prefix='/mobile_index')
-
-
-@mobile_index_bp.route('/')
-def mobile_index():
-    return render_template('mobile_index.html')
-
